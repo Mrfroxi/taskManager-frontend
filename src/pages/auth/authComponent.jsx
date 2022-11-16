85bf8392e4200e046eaea0c29cb17f0272b01521
Mrfroxi <froxi340@mail.ru>
2022-11-16 22:49:05 +0300

    fix:fixed bug with styled div

--- src/pages/auth/authComponent.jsx
+++ src/pages/auth/authComponent.jsx
@@ -1,9 +1,19 @@
 import React ,{useState} from 'react';
-import classes from  './authComponent.module.css';
-import { createGlobalStyle } from 'styled-components';
+import   styled, { createGlobalStyle } from 'styled-components';
 import { useInput } from '../../shared/utils/useInput';
 import AuthSection from '../../wrapper/authFormSectionWrapper/authSectionWrapper';
 import AuthFormBox from '../../wrapper/authFormWrapper/authFormWrapper';
+
+const StyledComponent = styled.div`
+   width:800px;
+   height: 500px;
+   padding: 40px 0;
+   position: relative;
+   box-shadow:  0 5px 45px rgba(0, 0, 0, 0.2);
+   display: flex;
+   align-items: center;
+   overflow: hidden;
+`;
 
 const AuthComponent =  () => {
 	const email = useInput('', {isEmpty:true ,minLength:5,isEmail: true ,} );
@@ -23,25 +33,27 @@ const AuthComponent =  () => {
 		justify-content: center;
 		align-items: center; 
 		transition:all 0.5s ease;
-		/* background: ${props => (props.active ? 'linear-gradient(45deg,rgb(31,142,190),#7d058f,rgb(68,4,149))' : 'linear-gradient(45deg,rgb(79, 26, 80),rgb(31,142,190),rgb(68,4,149))')}; */
 		background: ${props => props.active ? 'rgb(68,4,149)' :'rgb(31,142,190)'}; 
+	}
+
+	*{
+    box-sizing: border-box;
 	}
 	`;
 
-	return (
-		<div className={classes.formContainer}>
-			<GlobalStyle active={currentAuthCondition}/>
+	return (<StyledComponent >
+		<GlobalStyle active={currentAuthCondition}/>
 
-			<AuthSection onClick ={() => changeAuth() }/>
+		<AuthSection onClick ={() => changeAuth() }/>
 
-			<AuthSection onClick ={() => changeAuth() }/>
+		<AuthSection onClick ={() => changeAuth() }/>
 		
-		 	<AuthFormBox   active={currentAuthCondition} auth={email} type={'reg'}/>
+		<AuthFormBox   active={currentAuthCondition} auth={email} type={'reg'}/>
 
-			<AuthFormBox   active={currentAuthCondition} auth={password} type={'log'}/> 
+		<AuthFormBox   active={currentAuthCondition} auth={password} type={'log'}/>  
 		
-		</div>
-	);
+	</StyledComponent>)
+	;
 };
 
 export default AuthComponent;
